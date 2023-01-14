f522a228ee452fc19e11c91931e7d35310ce5271
J-MRodriguez <87613058+J-MRodriguez@users.noreply.github.com>
2023-01-14 17:33:28 -0300

    comenzando el manejo de estados

--- client/src/index.js
+++ client/src/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import {} from './redux/store';
+import { persistor, store } from './redux/store';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 	<React.StrictMode>
-		<Provider>
-			<PersistGate>
+		<Provider store={store}>
+			<PersistGate loading={'loading'} persistor={persistor}>
 				<App />
 			</PersistGate>
 		</Provider>
