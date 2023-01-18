408f9c8ab4007e3e2acffaefea2cee7881daf5cb
J-MRodriguez <87613058+J-MRodriguez@users.noreply.github.com>
2023-01-17 23:32:58 -0300

    contact and featuredProducts

--- client/src/components/Contact/Contact.jsx
+++ client/src/components/Contact/Contact.jsx
@@ -1,5 +1,29 @@
+import FacebookIcon from '@mui/icons-material/Facebook';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import GoogleIcon from '@mui/icons-material/Google';
+import PinterestIcon from '@mui/icons-material/Pinterest';
+import './Contact.scss';
+
 const Contact = () => {
-	return <div>Contact</div>;
+	return (
+		<div className='contact'>
+			<div className='wrapper'>
+				<span>Be in touch with us:</span>
+				<div className='mail'>
+					<input type='text' placeholder='Enter your e-mail...' />
+					<button>Join Us</button>
+				</div>
+				<div className='icons'>
+					<FacebookIcon />
+					<InstagramIcon />
+					<TwitterIcon />
+					<GoogleIcon />
+					<PinterestIcon />
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Contact;
--- client/src/components/FeaturedProducts/FeaturedProducts.jsx
+++ client/src/components/FeaturedProducts/FeaturedProducts.jsx
@@ -1,5 +1,33 @@
-const FeaturedProducts = () => {
-	return <div>FeaturedProducts</div>;
+import useFetch from '../../hooks/useFetch';
+import './FeaturedProducts.scss';
+
+const FeaturedProducts = ({ type }) => {
+	const { data, loading, error } = useFetch(
+		`/products?populate=*&[filters][type][$eq]=${type}`
+	);
+
+	return (
+		<div className='featuredProducts'>
+			<div className='top'>
+				<h1>{type} products</h1>
+				<p>
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum sit
+					veritatis soluta iusto tempora iure accusantium at sequi excepturi cum
+					autem, culpa molestias eius voluptatem sint eaque molestiae
+					exercitationem pariatur. Lorem, ipsum dolor sit amet consectetur
+					adipisicing elit. Architecto ratione aperiam error odio dolor quidem
+					eveniet inventore pariatur eos consequatur, repellat blanditiis vitae
+					dicta autem. Amet illo non cupiditate voluptatem.
+				</p>
+			</div>
+			<div className='bottom'>
+				{error ? 'Something went wrong!' : loading}
+				{loading
+					? 'loading'
+					: data?.map((item) => <div key={item.id}>{item.id}</div>)}
+			</div>
+		</div>
+	);
 };
 
 export default FeaturedProducts;
