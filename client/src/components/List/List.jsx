9f50b31c2a8d20369ec1f8e88f8f09b68d872272
J-MRodriguez <87613058+J-MRodriguez@users.noreply.github.com>
2023-01-21 12:03:33 -0300

    se incorpora list y navbar

--- client/src/components/List/List.jsx
+++ client/src/components/List/List.jsx
@@ -1,5 +1,21 @@
-const List = () => {
-	return <div>List</div>;
+import useFetch from '../../hooks/useFetch';
+import Card from '../Card/Card';
+import './List.scss';
+
+const List = ({ subCats, maxPrice, sort, catId }) => {
+	const { data, loading } = useFetch(
+		`/products?populate=*&[filters][categories][id]=${catId}${subCats.map(
+			(item) => `&[filters][sub_categories][id][$eq]=${item}`
+		)}&[filters][price][$lte]=${maxPrice}&sort=price:${sort}`
+	);
+
+	return (
+		<div className='list'>
+			{loading
+				? 'loading'
+				: data?.map((item) => <Card item={item} key={item.id} />)}
+		</div>
+	);
 };
 
 export default List;
