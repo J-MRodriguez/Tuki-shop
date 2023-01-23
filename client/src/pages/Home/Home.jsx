8e32d7d678ae52b30cefb97976b45bf293382e10
J-MRodriguez <87613058+J-MRodriguez@users.noreply.github.com>
2023-01-23 00:20:05 -0300

    cambios en el home

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,5 +1,18 @@
+import Categories from '../../components/Categories/Categories';
+import Contact from '../../components/Contact/Contact';
+import FeaturedProducts from '../../components/FeaturedProducts/FeaturedProducts';
+import Slider from '../../components/Slider/Slider';
+
 const Home = () => {
-	return <div>Home</div>;
+	return (
+		<div className='home'>
+			<Slider />
+			<FeaturedProducts type='featured' />
+			<Categories />
+			<FeaturedProducts type='trending' />
+			<Contact />
+		</div>
+	);
 };
 
 export default Home;
